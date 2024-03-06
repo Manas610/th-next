79f3997d891d2fac28b1bdc0ff6362925ee93c37
Manas <singhmanas610@gmail.com>
2024-03-06 14:18:58 +0530

    contact us route added

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,11 @@
+function Contact() {
+  return (
+    <div className="min-h-screen mt-16">
+      <div className="flex mx-auto py-36 justify-center">
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Contact <span className="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">us</span></h1>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,9 +39,9 @@ function Navbar() {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M1 1h15M1 7h15M1 13h15"
               />
             </svg>
@@ -54,7 +54,7 @@ function Navbar() {
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-black/[0.96] dark:border-gray-700">
             <li>
               <Link
-                href="#"
+                href="/"
                 className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                 aria-current="page"
               >
@@ -79,7 +79,7 @@ function Navbar() {
             </li>
             <li>
               <Link
-                href="#"
+                href="/contact-us"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Contact
